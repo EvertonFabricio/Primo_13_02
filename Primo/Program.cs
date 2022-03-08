8aa5ed26967a1e5bb42c214e91b9e24e97e0a835
EvertonFabricio <everton.5by5@gmail.com>
2022-03-08 10:04:23 -0300

    Adicionar arquivos de projeto.

---
+++ Primo/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Primo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int compara=0;
+
+            Console.WriteLine("Digite um numero qualquer: ");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 2;i < n; i++)
+            {
+                if (n%i == 0)
+                    compara ++;
+            }
+
+            if (compara == 0)
+                Console.WriteLine($"\n{n} é um numero primo!!");
+            else
+                Console.WriteLine($"\n{n} não é um numero primo.\n\n");
+        }
+    }
+}
